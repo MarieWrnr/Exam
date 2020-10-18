686c2842e7c85c4ebfefc8714f2b49a8de4b79ff
Maria <mv.matsko@gmail.com>
2020-10-18 15:13:11 +0300

    index.html is full and added new details for scss

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+window.addEventListener('scroll', handleFixedHeader);
+
+function handleFixedHeader() {
+  const $header = document.querySelector('.header');
+  const { scrollY } = window;
+  const fixedPos = 100;
+
+  if (scrollY > fixedPos) {
+    $header.classList.add('is-fixed');
+  } else {
+    $header.classList.remove('is-fixed');
+  }
+}
